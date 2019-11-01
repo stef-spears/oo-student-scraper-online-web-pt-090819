c41308f1947f95656b174492fc22d03e4a97f5ce
Stef Spears <spears.sj87@gmail.com>
2019-11-01 18:02:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,13 +16,13 @@ class Student
   def self.create_from_collection(students_array)
     
     students_array.each do |student|
-      #binding.pry
+      
       self.new(student)
     end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    binding.pry
   end
 
   def self.all
